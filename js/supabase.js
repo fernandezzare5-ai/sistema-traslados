30299eb7039a078f27e1bfaaa45273ae72519f13
fernandezzare5-ai <fernandezzare.5@gmail.com>
2026-08-30 23:51:37 -0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,2 +1,21 @@
-// Conexión Supabase
-// Se configurará después
+// =====================================
+// CONEXIÓN SUPABASE
+// SISTEMA DE TRASLADOS
+// =====================================
+
+
+const SUPABASE_URL = 
+"https://vmuyuwduumhqaejrslnt.supabase.co";
+
+
+const SUPABASE_KEY =
+"sb_publishable_pYIS2a8P3ydhbZC4Fzn7qg_KMw62YHn";
+
+
+
+// Crear conexión
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
